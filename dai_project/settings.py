dfc687c7f06d4dc35ca6326a938e0e931969dcbc
Anders Hofstee <a.hofstee@catalpainternational.org>
2012-09-19 10:46:42 +0900

    added GRAPPELLI_ADMIN_TITLE

--- dai_project/settings.py
+++ dai_project/settings.py
@@ -11,6 +11,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+GRAPPELLI_ADMIN_TITLE = u"Market Survey"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
